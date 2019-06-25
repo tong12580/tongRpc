02631a985810630ef19ec4cfd4374efed2c4d024
tong12580 <467942250@hotmail.com>
2019-06-25 21:49:17 +0800

    Rpc三注解

---
+++ tong12580.rpc/src/main/java/io/github/tong12580/rpc/core/lang/RpcClient.java
@@ -0,0 +1,14 @@
+package io.github.tong12580.rpc.core.lang;
+
+/**
+ * <p>RpcClient</p>
+ * <span>Rpc客户端</span>
+ *
+ * <span>被该标记标标注的将成为Rpc客户端, 具有Rpc相关调用能力</span>
+ *
+ * @author yuTong
+ * @version 1.0
+ * @since 2019/6/25 21:39
+ */
+public @interface RpcClient {
+}
---
+++ tong12580.rpc/src/main/java/io/github/tong12580/rpc/core/lang/RpcServer.java
@@ -0,0 +1,14 @@
+package io.github.tong12580.rpc.core.lang;
+
+/**
+ * <p>RpcServer</p>
+ * <span>对外提供RPC服务标签</span>
+ *
+ * <span>被该标记标标注的将成为Rpc客户端, 具有提供Rpc服务能力</span>
+ *
+ * @author yuTong
+ * @version 1.0
+ * @since 2019/6/25 21:37
+ */
+public @interface RpcServer {
+}
